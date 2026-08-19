314730424f5a1650b5ce198f7b24fe16358a6113
“mlrlima” <mlrlima04@gmail.com>
2026-08-18 23:09:08 -0300

    adicionando alguns edits

---
+++ edits/edits-db.js
@@ -0,0 +1,83 @@
+const edits = [
+    /*
+    {
+        imagem: "db/ano/nome.png",
+        titulo: "",
+        tags: [],
+
+        ano: 0,
+        mes: 0,
+        dia: 0,
+    },
+    */
+
+/* 
+DEC 2025 
+*/
+    
+    {
+        imagem: "db/2025/lay-still-restless.jpg",
+        titulo: "Lay still, restless",
+        tags: ["song: My Ordinary Life - The Living Tombstone"],
+
+        ano: 2026,
+        mes: 12,
+        dia: 5,
+    },
+
+/* 
+NOV 2025 
+*/
+
+    {
+        imagem: "db/2025/primeiro_hamster.png",
+        titulo: "Primeiro feito no Affinity",
+        tags: [],
+
+        ano: 2026,
+        mes: 11,
+        dia: 16,
+    },
+
+/* 
+APR 2019
+*/
+
+    {
+        imagem: "db/2019/theme-bright-ariana.png",
+        titulo: "",
+        tags: ["Ariana Grande", 
+            "song: One Last Time - Ariana Grande"],
+
+        ano: 2019,
+        mes: 4,
+        dia: 23,
+    },
+
+    {
+        imagem: "db/2019/when-you-left-i-felt-like-dying.png",
+        titulo: "when you left I felt like dying",
+        tags: [],
+
+        ano: 2019,
+        mes: 4,
+        dia: 11,
+    },
+
+/*
+AUG 2018
+*/
+
+    {
+        imagem: "db/2018/complex-white-ariana.png",
+        titulo: "",
+        tags: ["Ariana Grande", 
+            "song: sweetener - Ariana Grande",
+            "complex"
+        ],
+
+        ano: 2018,
+        mes: 8,
+        dia: 22,
+    },
+];
