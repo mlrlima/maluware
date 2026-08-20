6dcac2eeaeb3559e1ba0602e5cce2091ec6fbca8
“mlrlima” <mlrlima04@gmail.com>
2026-08-19 22:51:38 -0300

    adicionando mais alguns edits

--- edits/script.js
+++ edits/script.js
@@ -1,7 +1,7 @@
 const edits = [
     /*
     {
-        imagem: "db/ano/nome.png",
+        imagem: "db/ano/mes/nome.png",
         titulo: "",
         tags: [],
 
@@ -16,7 +16,7 @@ DEC 2025
 */
     
     {
-        imagem: "db/2025/lay-still-restless.jpg",
+        imagem: "db/2025/dezembro/lay-still-restless.jpg",
         titulo: "Lay still, restless",
         tags: ["song: My Ordinary Life - The Living Tombstone"],
 
@@ -30,7 +30,7 @@ NOV 2025
 */
 
     {
-        imagem: "db/2025/primeiro_hamster.png",
+        imagem: "db/2025/novembro/primeiro_hamster.png",
         titulo: "Primeiro feito no Affinity",
         tags: [],
 
@@ -44,7 +44,46 @@ APR 2019
 */
 
     {
-        imagem: "db/2019/theme-bright-ariana.png",
+        imagem: "db/2019/abril/complex-vaporwave-twice.png",
+        titulo: "",
+        tags: ["Twice",
+            "complex",
+            "song: Fancy - Twice",
+            "Dahyun","Sana", "Chaeyoung",
+            "Jihyo", "Mina", "Jeongyeon",
+            "Momo", "Tzuyu",
+            "por algum motivo nao coloquei a nayeon nesse edit??? era minha bias nessa epoca"
+        ],
+
+        ano: 2019,
+        mes: 4,
+        dia: 30,
+    },
+
+    {
+        imagem: "db/2019/abril/cap-marvel-ww.png",
+        titulo: "",
+        tags: ["Captain Marvel",
+            "Wonder Woman"
+        ],
+
+        ano: 2019,
+        mes: 4,
+        dia: 28,
+    },
+
+    {
+        imagem: "db/2019/abril/momoland.png",
+        titulo: "",
+        tags: ["Momoland"],
+
+        ano: 2019,
+        mes: 4,
+        dia: 25,
+    },
+
+    {
+        imagem: "db/2019/abril/theme-bright-ariana.png",
         titulo: "",
         tags: ["Ariana Grande", 
             "song: One Last Time - Ariana Grande"],
@@ -55,7 +94,69 @@ APR 2019
     },
 
     {
-        imagem: "db/2019/when-you-left-i-felt-like-dying.png",
+        imagem: "db/2019/abril/theme-bright-jisoo-yeri.png",
+        titulo: "",
+        tags: ["Jisoo", "Blackpink",
+            "Yeri", "Red Velvet",
+            "song: My Favorite Part - Mac Miller (feat. Ariana Grande)"
+        ],
+
+        ano: 2019,
+        mes: 4,
+        dia: 21,
+    },
+
+    {
+        imagem: "db/2019/abril/theme-bright-jisoo-yeri-sana.png",
+        titulo: "",
+        tags: ["Jisoo", "Blackpink",
+            "Yeri", "Red Velvet",
+            "Sana", "Twice"
+        ],
+
+        ano: 2019,
+        mes: 4,
+        dia: 21,
+    },
+
+    {
+        imagem: "db/2019/abril/vaporwave-futaba-persona.png",
+        titulo: "",
+        tags: ["Futaba", "Persona 5",
+            "7 anos depois descobri q eh uma personagem de persona kkkkk"
+        ],
+
+        ano: 2019,
+        mes: 4,
+        dia: 19,
+    },
+
+    {
+        imagem: "db/2019/abril/vaporwave-menina.png",
+        titulo: "",
+        tags: [],
+
+        ano: 2019,
+        mes: 4,
+        dia: 16,
+    },
+
+    {
+        imagem: "db/2019/abril/complex-umaru-zerotwo-kaede-momo.png",
+        titulo: "",
+        tags: ["Umaru-chan", "Himouto! Umaru-chan",
+            "Zero Two", "Darling in the FranXX",
+            "Kaede", "Rascal does not dream of bunny girl sempai",
+            "Momo", "Boku no hero academia"
+        ],
+
+        ano: 2019,
+        mes: 4,
+        dia: 13,
+    },
+
+    {
+        imagem: "db/2019/abril/when-you-left-i-felt-like-dying.png",
         titulo: "when you left I felt like dying",
         tags: [],
 
@@ -64,12 +165,220 @@ APR 2019
         dia: 11,
     },
 
+    {
+        imagem: "db/2019/abril/angelina-capozzoli.png",
+        titulo: "",
+        tags: ["Angelina Capozzoli", 
+            "song: ghostin - Ariana Grande"
+        ],
+
+        ano: 2019,
+        mes: 4,
+        dia: 9,
+    },
+
+    {
+        imagem: "db/2019/abril/green-nature.png",
+        titulo: "",
+        tags: [],
+
+        ano: 2019,
+        mes: 4,
+        dia: 7,
+    },
+    {
+        imagem: "db/2019/abril/jennie-kiss-and-make-up.png",
+        titulo: "",
+        tags: ["Jennie", "Blackpink",
+            "song: Kiss and Make Up - BLACKPINK, Dua Lipa"
+        ],
+
+        ano: 2019,
+        mes: 4,
+        dia: 4,
+    },
+
+    {
+        imagem: "db/2019/abril/quilava.png",
+        titulo: "Quilava",
+        tags: ["Quilava","Cyndaquil", "Pokemon"],
+
+        ano: 2019,
+        mes: 4,
+        dia: 2,
+    },
+
+/* 
+MAR 2019
+*/
+
+    {
+        imagem: "db/2019/marco/bubbline-wallpaper-1.png",
+        titulo: "marceline + jujuba wallpaper <3",
+        tags: ["Marceline", 
+            "Princesa Jujuba", "Princess Bubblegum",
+            "Hora de aventura", "Adventure Time",
+            "wallpaper"
+        ],
+
+        ano: 2019,
+        mes: 3,
+        dia: 31,
+    },
+
+    {
+        imagem: "db/2019/marco/bubbline-wallpaper-2.png",
+        titulo: "marceline + jujuba wallpaper <3",
+        tags: ["Marceline", 
+            "Princesa Jujuba", "Princess Bubblegum",
+            "Hora de aventura", "Adventure Time",
+            "wallpaper"
+        ],
+
+        ano: 2019,
+        mes: 3,
+        dia: 31,
+    },
+
+    {
+        imagem: "db/2019/marco/kirishima.png",
+        titulo: "Kirishima",
+        tags: ["Kirishima", "Boku no hero academia"],
+
+        ano: 2019,
+        mes: 3,
+        dia: 28,
+    },
+
+    {
+        imagem: "db/2019/marco/noragami.png",
+        titulo: "",
+        tags: ["Noragami"],
+
+        ano: 2019,
+        mes: 3,
+        dia: 26,
+    },
+
+    {
+        imagem: "db/2019/marco/yellow-meliodas.png",
+        titulo: "",
+        tags: ["Meliodas", "Nanatsu no taizai"],
+
+        ano: 2019,
+        mes: 3,
+        dia: 23,
+    },
+
+    {
+        imagem: "db/2019/marco/complex-pink-gowther.png",
+        titulo: "Gowther",
+        tags: ["Gowther", "Nanatsu no taizai"],
+
+        ano: 2019,
+        mes: 3,
+        dia: 20,
+    },
+
+    {
+        imagem: "db/2019/marco/anatomy-mina.png",
+        titulo: "Anatomy edit da Mina",
+        tags: ["Mina", "Twice",
+            "Anatomy"
+        ],
+
+        ano: 2019,
+        mes: 3,
+        dia: 18,
+    },
+
+    {
+        imagem: "db/2019/marco/festa-do-cha-de-bosta.png",
+        titulo: "festa do chá de bosta",
+        tags: ["Moodboard"],
+
+        ano: 2019,
+        mes: 3,
+        dia: 16,
+    },
+
+    {
+        imagem: "db/2019/marco/nasa-ariana.png",
+        titulo: "",
+        tags: ["Ariana Grande",
+            "song: NASA - Ariana Grande"
+        ],
+
+        ano: 2019,
+        mes: 3,
+        dia: 16,
+    },
+
+    {
+        imagem: "db/2019/marco/pink-blackpink.png",
+        titulo: "",
+        tags: ["Jisoo", "Jennie", "Lisa", "Rose",
+            "Blackpink"
+        ],
+
+        ano: 2019,
+        mes: 3,
+        dia: 14,
+    },
+
+    {
+        imagem: "db/2019/marco/green-tsuyu-bnha.png",
+        titulo: "Asui Tsuyu",
+        tags: ["Froppy","Tsuyu", 
+            "Boku no hero academia"
+        ],
+
+        ano: 2019,
+        mes: 3,
+        dia: 11,
+    },
+
+    {
+        imagem: "db/2019/marco/pink-sana.png",
+        titulo: "",
+        tags: ["Sana", "Twice",
+            "song: Brand New Girl - Twice"
+        ],
+
+        ano: 2019,
+        mes: 3,
+        dia: 11,
+    },
+
+    {
+        imagem: "db/2019/marco/white-norman-tpn.png",
+        titulo: "",
+        tags: ["Norman", "The Promised Neverland"
+        ],
+
+        ano: 2019,
+        mes: 3,
+        dia: 8,
+    },
+    
+    {
+        imagem: "db/2019/marco/ariana-7rings.png",
+        titulo: "",
+        tags: ["Ariana Grande",
+            "song: 7 rings - Ariana Grande"
+        ],
+
+        ano: 2019,
+        mes: 3,
+        dia: 6,
+    },
+
 /*
 AUG 2018
 */
 
     {
-        imagem: "db/2018/complex-white-ariana.png",
+        imagem: "db/2018/agosto/complex-white-ariana.png",
         titulo: "",
         tags: ["Ariana Grande", 
             "song: sweetener - Ariana Grande",
@@ -226,7 +535,7 @@ function mostrarGaleria() {
 
     /*Pagination*/
 
-    paginaTexto.textContent=`${paginaAtual} / ${totalPaginas}`;
+    paginaTexto.textContent=`${paginaAtual} de ${totalPaginas}`;
 
     botaoAnterior.disabled = paginaAtual === 1;
 
